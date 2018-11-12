909664d9243d00ec54d1ff3ac62f6f79f28d2e35
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-11-12 17:27:36 +0200

    Print student's submissions

--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,7 +1,13 @@
 package ohtu;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Submission {
     private int week;
+    private int hours;
+    private int[] exercises;
+    private String course;
 
     public void setWeek(int week) {
         this.week = week;
@@ -11,9 +17,34 @@ public class Submission {
         return week;
     }
 
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setExercises(int... exercises) {
+        this.exercises = exercises;
+    }
+
+    public int[] getExercises() {
+        return exercises;
+    }
+    
+    public void setHours(String course) {
+        this.course = course;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+    
+
     @Override
     public String toString() {
-        return ""+week;
+        return course+", viikolla "+week+" tehdyt tehtävät: "+String.join(",", Arrays.stream(exercises).sorted().mapToObj(Integer::toString).collect(Collectors.toList()))+" (aikaa käytetty "+hours+" tuntia)";
     }
     
 }
