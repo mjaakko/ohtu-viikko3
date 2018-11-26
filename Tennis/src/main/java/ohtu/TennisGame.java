a3369f7a628edc39dd295e5408b35050582b8f6d
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-11-26 19:50:47 +0200

    Refactoring

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,9 +1,11 @@
 package ohtu;
 
 public class TennisGame {
+    private static final int ADVANTAGE_THRESHOLD = 1;
+    private static final int WINNING_THRESHOLD = 1;
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1score = 0;
+    private int player2score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,68 +15,63 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1Name)) {
+            player1score += 1;
+        }
+        if (playerName.equals(player2Name)) {
+            player2score += 1;
+        }
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (player1score == player2score) {
+            return getScoreWhenTied(player2score);
+        } else if (player1score>=4 || player2score>=4)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            return getScoreWhenWinning();
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            return getScoreNormal();
         }
-        return score;
+    }
+    
+    private String getScoreWhenTied(int score) {
+        String text = getScoreText(score);
+        
+        return text == null ? "Deuce" : text+"-All";
+    }
+    
+    private String getScoreWhenWinning() {
+        if (player1score - player2score == ADVANTAGE_THRESHOLD) {
+            return "Advantage player1";
+        } else if (player2score - player1score == ADVANTAGE_THRESHOLD) {
+            return "Advantage player2";
+        } else if (player1score - player2score >= WINNING_THRESHOLD) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
+    }
+    
+    private String getScoreNormal() {
+        return getScoreText(player1score)+"-"+getScoreText(player2score);
+    }
+    
+    private String getScoreText(int score) {
+        switch(score)  {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return null;
+        }
     }
 }
