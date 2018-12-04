a82efb29053548140811464320dd205b99d01e33
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-12-04 19:18:44 +0200

    Refactor Laskin

--- Laskin/src/main/java/laskin/Sovelluslogiikka.java
+++ Laskin/src/main/java/laskin/Sovelluslogiikka.java
@@ -1,22 +1,34 @@
 package laskin;
 
+import java.util.ArrayDeque;
+
 public class Sovelluslogiikka {
- 
+    private ArrayDeque<Integer> historia = new ArrayDeque<>();
+    
     private int tulos;
  
     public void plus(int luku) {
+        historia.push(tulos);
         tulos += luku;
     }
      
     public void miinus(int luku) {
+        historia.push(tulos);
         tulos -= luku;
     }
  
     public void nollaa() {
+        historia.push(tulos);
         tulos = 0;
     }
  
     public int tulos() {
         return tulos;
+    }
+    
+    public void undo() {
+        if (!historia.isEmpty()) {
+            tulos = historia.pop();
+        }
     }
 }
--- Laskin/src/main/java/laskin/Tapahtumankuuntelija.java
+++ Laskin/src/main/java/laskin/Tapahtumankuuntelija.java
@@ -1,5 +1,7 @@
 package laskin;
 
+import java.util.HashMap;
+import java.util.Map;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
@@ -8,52 +10,48 @@ import javafx.scene.control.TextField;
 public class Tapahtumankuuntelija implements EventHandler {
     private TextField tuloskentta; 
     private TextField syotekentta; 
-    private Button plus;
-    private Button miinus;
+    
+    private Map<Button, Komento> komennot = new HashMap<>();
+    
     private Button nollaa;
     private Button undo;
+    
     private Sovelluslogiikka sovellus;
 
     public Tapahtumankuuntelija(TextField tuloskentta, TextField syotekentta, Button plus, Button miinus, Button nollaa, Button undo) {
         this.tuloskentta = tuloskentta;
         this.syotekentta = syotekentta;
-        this.plus = plus;
-        this.miinus = miinus;
         this.nollaa = nollaa;
         this.undo = undo;
+        komennot.put(plus, (tila) -> tila.plus(getInput()));
+        komennot.put(miinus, (tila) -> tila.miinus(getInput()));
+        komennot.put(nollaa, (tila) -> tila.nollaa());
+        komennot.put(undo, (tila) -> tila.undo());
         this.sovellus = new Sovelluslogiikka();
     }
     
     @Override
     public void handle(Event event) {
-        int arvo = 0;
- 
-        try {
-            arvo = Integer.parseInt(syotekentta.getText());
-        } catch (Exception e) {
-        }
- 
-        if (event.getTarget() == plus) {
-            sovellus.plus(arvo);
-        } else if (event.getTarget() == miinus) {
-            sovellus.miinus(arvo);
-        } else if (event.getTarget() == nollaa) {
-            sovellus.nollaa();
-        } else {
-            System.out.println("undo pressed");
-        }
+        komennot.get(event.getTarget()).suorita(sovellus);
         
         int laskunTulos = sovellus.tulos();
         
         syotekentta.setText("");
         tuloskentta.setText("" + laskunTulos);
         
-        if ( laskunTulos==0) {
+        if (laskunTulos == 0) {
             nollaa.disableProperty().set(true);
         } else {
             nollaa.disableProperty().set(false);
         }
         undo.disableProperty().set(false);
     }
-
+    
+    private int getInput() {
+        try {
+            return Integer.parseInt(syotekentta.getText());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
 }
